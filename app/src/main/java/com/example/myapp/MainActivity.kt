14552a402031fda92449b50c82afff7e549db294
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2022-10-06 02:51:08 +0600

    Reactive Property Added

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,17 +3,37 @@ package com.example.myapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mViewModel: MyAppViewModel
     private lateinit var name: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.lifecycleOwner = this
-        binding.name="Jishan"
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.lifecycleOwner = this
+        mViewModel = ViewModelProvider(this).get(MyAppViewModel::class.java)
+        // mBinding.tvName.text = mViewModel.getList()[0]
+
+        subscribeUi()
+
     }
 
+    private fun populateUi(list: List<String>) {
+        mBinding.tvName.text = list[0]
+    }
 
+    private fun subscribeUi() {
+        subscribeUiToReactiveList(mViewModel.getReactiveList())
+    }
+
+    private fun subscribeUiToReactiveList(liveData: LiveData<List<String>>) {
+        liveData.observe(this, Observer {
+            populateUi(it)
+        })
+    }
 }
--- app/src/main/java/com/example/myapp/MyAppViewModel.kt
+++ app/src/main/java/com/example/myapp/MyAppViewModel.kt
@@ -2,8 +2,19 @@ package com.example.myapp
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class MyAppViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val reactiveList: MutableLiveData<List<String>> = MutableLiveData<List<String>>()
+    private var list:ArrayList<String> = ArrayList()
+
+    init {
+        list.add("Hasnath Jami Chowdhury")
+        list.add("Sayem")
+        reactiveList.value=list
+    }
 
     private lateinit var name: String
 
@@ -11,7 +22,16 @@ class MyAppViewModel(application: Application) : AndroidViewModel(application) {
         this.name = name;
     }
 
-    private fun getName(): String = name
+    public fun getName(): String = name
+
+
+    public fun getList(): ArrayList<String> {
+        return list;
+    }
+
+    public fun getReactiveList(): LiveData<List<String>> {
+        return reactiveList;
+    }
 
 
 }
